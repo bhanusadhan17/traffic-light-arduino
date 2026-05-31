4239872d1d25efb1f3108b9f0793d23038dfd2ed
bhanusadhan17 <sadhanbuddha@gmail.com>
2026-05-31 21:11:50 +0530

    Add traffic light code

---
+++ traffic_light.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+}
+void loop()
+{
+  digitalWrite(10,HIGH);
+  delay(1000);
+  digitalWrite(10,LOW);
+  delay(1000);
+  
+  digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(9,LOW);
+  delay(1000);
+  
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+  delay(1000);
+}
